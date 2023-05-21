05e6d9e6bb7ee545dfad5b76bd70efd2bb65859c
Your Name <lucasdosualdo@gmail.com>
2023-05-21 16:39:34 -0300

    feat: read candidate data by id

--- app/Http/Controllers/CandidateController.php
+++ app/Http/Controllers/CandidateController.php
@@ -5,13 +5,20 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Candidate;
 
-class SignUpController extends Controller
+class CandidateController extends Controller
 {
     public function signUpCandidate(Request $request) {
        Candidate::create([
         'name'=>$request->candidate_name,
         'phone'=>$request->candidate_phone
        ]);
-       echo "deu bom";
+       echo "success";
+    }
+
+    public function showCandidate ($candidate_id) {
+        $candidate = Candidate::findOrFail($candidate_id);
+        echo $candidate->name;
+        echo "<br/>";
+        echo $candidate->phone;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,23 +2,13 @@
 
 use Illuminate\http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Models\Candidate;
-use App\Http\Controllers\SignUpController;
 
+use App\Http\Controllers\CandidateController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
     Route::get('/', function () {
         return view('welcome');
     });
 
-    Route::post('/signup-candidate', [SignUpController::class, 'signUpCandidate']);
+    Route::post('/signup-candidate', [CandidateController::class, 'signUpCandidate']);
+    Route::get('/show-candidate/{candidate_id}', [CandidateController::class, 'showCandidate']);
