bc9503b04864f8c668b00a67fa2c65cb05c861e0
Your Name <lucasdosualdo@gmail.com>
2023-05-21 10:55:27 -0300

    feat: migrate candidates schema to database

--- database/migrations/2023_05_21_133834_create_candidates_table.php
+++ database/migrations/2023_05_21_133834_create_candidates_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('candidates', function (Blueprint $table) {
             $table->id();
-            $table->phone();
+            $table->string('phone');
             $table->timestamps();
         });
     }
